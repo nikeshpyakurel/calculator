4d5616b643147e98ad76853742171ed821c530a4
Nikesh Pyakurel <nikeshpyakurel32@gmail.com>
2023-06-05 18:07:35 +0545

    all done

--- calc.py
+++ calc.py
@@ -2,10 +2,13 @@ from _ast import Lambda
 from tkinter import *
 from math import *
 root = Tk()
+root.resizable(0, 0)
+
+root.config(background="#ffffff")
 
 
 # defining title of the project
-root.title("Simple Calculator")
+root.title("Scientific Calculator")
 
 
 # button click
@@ -74,87 +77,102 @@ exps = ""
 exp_txt = StringVar()
 # Defining the buttons
 
-e = Entry(root, width=35, textvariable=exp_txt, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
+e = Entry(root, font=('poppins', 26), width=15,
+          textvariable=exp_txt, borderwidth=0, highlightthickness=1, highlightcolor="#fff", relief="solid")
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=20)
 
 
-button_1 = Button(root, text="1", padx=40, pady=20,
-                  command=lambda: button_click(1))
-button_2 = Button(root, text="2", padx=40, pady=20,
-                  command=lambda: button_click(2))
-button_3 = Button(root, text="3", padx=40, pady=20,
-                  command=lambda: button_click(3))
-button_4 = Button(root, text="4", padx=40, pady=20,
-                  command=lambda: button_click(4))
-button_5 = Button(root, text="5", padx=40, pady=20,
-                  command=lambda: button_click(5))
-button_6 = Button(root, text="6", padx=40, pady=20,
-                  command=lambda: button_click(6))
-button_7 = Button(root, text="7", padx=40, pady=20,
+button_7 = Button(root, background="#ffffff", activebackground="#ffffff", borderwidth=0, font=('bold'), text="7", padx=31, pady=15,
                   command=lambda: button_click(7))
-button_8 = Button(root, text="8", padx=40, pady=20,
+button_8 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'),  text="8", padx=31, pady=15,
                   command=lambda: button_click(8))
-button_9 = Button(root, text="9", padx=40, pady=20,
+button_9 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'),  text="9", padx=31, pady=15,
                   command=lambda: button_click(9))
-button_0 = Button(root, text="0", padx=40, pady=20,
-                  command=lambda: button_click(0))
-button_dot = Button(root, text=".", padx=40, pady=20,
-                    command=lambda: button_click('.'))
-button_add = Button(root, text="+", padx=39, pady=20,
-                    command=lambda: button_click("+"))
-button_subtract = Button(root, text="-", padx=39, pady=20,
-                         command=lambda: button_click("-"))
-button_multiply = Button(root, text="*", padx=39, pady=20,
-                         command=lambda: button_click("*"))
-button_divide = Button(root, text="/", padx=39, pady=20,
-                       command=lambda: button_click("/"))
-button_equal = Button(root, text="=", padx=91, pady=20,
-                      command=button_equal)
-button_clear = Button(root, text="Clear", padx=79, pady=20,
-                      command=button_clear)
-button_sin = Button(root, text="Sin", padx=40, pady=20,
-                    command=sin_calc
-                    )
-button_cos = Button(root, text="Cos", padx=40, pady=20,
-                    command=cos_calc
-                    )
-button_tan = Button(root, text="Tan", padx=40, pady=20,
-                    command=tan_calc
-                    )
-button_sqrt = Button(root, text="√", padx=40, pady=20,
-                     command=sqrtMethod
-                     )
-button_square = Button(root, text="^", padx=40, pady=20,
-                       command=lambda: button_click("**")
-                       )
-button_back = Button(root, text="⌫", padx=40, pady=20,
+button_back = Button(root, fg="#ffffff", activebackground="#FF702A", activeforeground="#ffffff", background="#FF702A", borderwidth=0, font=('bold'),  text="⌫", padx=30, pady=15,
                      command=back_space
                      )
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-button_0.grid(row=4, column=0)
-button_dot.grid(row=8, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
+button_3 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'),  text="3", padx=31, pady=15,
+                  command=lambda: button_click(3))
+button_4 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'), text="4", padx=31, pady=15,
+                  command=lambda: button_click(4))
+button_5 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'), text="5", padx=31, pady=15,
+                  command=lambda: button_click(5))
+button_6 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'), text="6", padx=31, pady=15,
+                  command=lambda: button_click(6))
 
-button_add.grid(row=5, column=0)
-button_subtract.grid(row=5, column=1)
-button_multiply.grid(row=5, column=2)
-button_divide.grid(row=6, column=0)
-button_sin.grid(row=6, column=1)
-button_cos.grid(row=6, column=2)
-button_tan.grid(row=7, column=0)
-button_sqrt.grid(row=7, column=1)
-button_square.grid(row=8, column=1)
-button_equal.grid(row=7, column=2)
-button_back.grid(row=8, column=2)
+
+button_1 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'), text="1", padx=31, pady=15,
+                  command=lambda: button_click(1))
+button_2 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'), text="2", padx=31, pady=15,
+                  command=lambda: button_click(2))
+button_0 = Button(root, background="#ffffff", activebackground="#ffffff",  borderwidth=0, font=('bold'), text="0", padx=31, pady=15,
+                  command=lambda: button_click(0))
+button_dot = Button(root, background="#ffffff", fg="#FF702A", borderwidth=0, font=('bold'), text=".", padx=37, pady=15,
+                    command=lambda: button_click('.'))
+
+
+button_add = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'), text="+", padx=34, pady=15,
+                    command=lambda: button_click("+"))
+button_subtract = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'),  text="-", padx=32, pady=15,
+                         command=lambda: button_click("-"))
+button_multiply = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'), text="x", padx=31, pady=15,
+                         command=lambda: button_click("*"))
+button_divide = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'),  text="/", padx=37, pady=15,
+                       command=lambda: button_click("/"))
+
+
+button_sin = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'),  text="sin", padx=26, pady=15,
+                    command=sin_calc
+                    )
+button_cos = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'),  text="cos", padx=26, pady=15,
+                    command=cos_calc
+                    )
+button_tan = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'),  text="tan", padx=26, pady=15,
+                    command=tan_calc
+                    )
+button_sqrt = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, text="√", padx=35, pady=15,
+                     command=sqrtMethod
+                     )
+
+
+button_square = Button(root, background="#ffffff", activebackground="#ffffff", activeforeground="#FF702A", fg="#FF702A", borderwidth=0, font=('bold'),  text="^", padx=30, pady=15,
+                       command=lambda: button_click("**")
+                       )
+button_equal = Button(root, background="#FF702A", justify="center", fg="#ffffff", borderwidth=0, font=('bold'),  text="=", padx=95, pady=15,
+                      command=button_equal)
+button_clear = Button(root, background="#ffffff", fg="#FF702A", activebackground="#ffffff", activeforeground="#FF702A", borderwidth=0, font=('bold'), text="AC", padx=22, pady=15,
+                      command=button_clear)
+
+button_7.grid(row=1, sticky="w", columnspan=1, rowspan=1, column=0)
+button_8.grid(row=1, sticky="w", column=1)
+button_9.grid(row=1, sticky="w", column=2)
+button_back.grid(row=1, sticky="e", column=3)
+
+button_3.grid(row=3, sticky="w", column=2)
+button_4.grid(row=2, sticky="w", column=1)
+button_5.grid(row=2, sticky="w", column=2)
+button_6.grid(row=2, sticky="w", column=0)
+
+button_1.grid(row=3, sticky="w", column=0)
+button_2.grid(row=3, sticky="w", column=1)
+button_0.grid(row=4, sticky="w", column=0)
+button_dot.grid(row=3, sticky="e", column=3)
+
+
+button_add.grid(row=2, sticky="e", column=3)
+button_subtract.grid(row=4, sticky="w", column=1)
+button_multiply.grid(row=4, sticky="w", column=2)
+button_divide.grid(row=4, sticky="e", column=3)
+
+button_sin.grid(row=5, sticky="w", column=0)
+button_cos.grid(row=5, sticky="w", column=1)
+button_tan.grid(row=5, sticky="w", column=2)
+button_sqrt.grid(row=5, sticky="e", column=3)
+
+button_square.grid(row=6, sticky="w", column=0)
+button_clear.grid(row=6, sticky="w", column=1)
+button_equal.grid(row=6, sticky="e", columnspan=4, column=0)
 
 
 root.mainloop()
